2b6b1ff86a813a32a1bad5cd4de32c35dc6e4fee
P Aditya Rao <adiarprrr23@gmail.com>
2024-07-13 16:45:44 +0530

    Initial Commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+
+  return (
+    <div className='w-10'>
+      <img src="https://img.lovepik.com/element/40174/0048.png_1200.png" alt="Logo" />
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header"
+import Footer from "./Footer/Footer"
+import Container from "./Container/Container"
+import LogoutBtn from "./Header/LogoutBtn"
+import AuthLayout from "./AuthLayout"
+import Logo from "./Logo"
+import PostCard from "./PostCard"
+import RTE from "./RTE"
+import Select from "./Select"
+import Login from "./Login"
+import Button from "./Button"
+import Input from "../components/Input"
+import PostForm from "../components/post-form/PostForm"
+import Signup from "./Signup"
+
+export {
+    Header,
+    Footer,
+    Container,
+    LogoutBtn,
+    AuthLayout,
+    Logo,
+    PostCard,
+    RTE,
+    Select,
+    Login,
+    Button,
+    Input,
+    PostForm,
+    Signup
+}
